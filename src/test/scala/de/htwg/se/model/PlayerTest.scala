6355fe2db6ff5da25db144227e7b9d91f127a78f
Mücahit D <muecahit1993@hotmail.de>
2020-11-06 11:37:34 +0100

    card model

---
+++ src/test/scala/de/htwg/se/model/PlayerTest.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.model
+
+
+import org.scalatest._
+
+
+class PlayerTest extends WordSpec with Matchers {
+  "A Player" when { "new" should {
+    val player = Player("Your Name")
+    "have a name"  in {
+      player.name should be("Your Name")
+    }
+    "have a nice String representation" in {
+      player.toString should be("Your Name")
+    }
+  }}
+
+
+}
