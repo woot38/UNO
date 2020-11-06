ad14dac31d230a1f7e5bf0fb8d4d41452c9841c2
Mücahit D <muecahit1993@hotmail.de>
2020-11-06 12:02:18 +0100

    card model test

--- src/test/scala/de/htwg/se/model/PlayerTest.scala
+++ src/test/scala/de/htwg/se/model/PlayerTest.scala
@@ -7,15 +7,15 @@ import org.scalatest.wordspec.AnyWordSpec
 class PlayerTest extends AnyWordSpec {
   "A player" when {
     "created" should {
-      val player = Player("Player1Name")
+      val player = Player("Your Name")
       "have a name" in {
-        player.name should be("Player1Name")
+        player.name should be("Your Name")
       }
       "and toString should look like" in {
-        player.toString should be("Player1Name")
+        player.toString should be("Your Name")
       }
       "when unapplied" in {
-        Player.unapply(player).get should be ("Player1Name")
+        Player.unapply(player).get should be ("Your Name")
       }
     }
   }
