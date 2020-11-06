dc41dbd8b5de0343c01def13c221b8fd67e95eaf
Mücahit D <muecahit1993@hotmail.de>
2020-11-06 12:15:54 +0100

    card model test

--- src/main/scala/UNO.scala
+++ src/main/scala/UNO.scala
@@ -4,5 +4,7 @@ object UNO {
   def main(args: Array[String]): Unit = {
     val player_1 = Player("Your Name")
     printf("Welcome player %s!!!", player_1)
+    val card_1 = Card(0,"green")
+
   }
 }
---
+++ src/test/scala/CardTest.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.model
+
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+class CardTest extends AnyWordSpec {
+  "A card" when {
+    "pulled" should {
+      val card = Card(0,"green")
+      "have a colour and colour" in {
+        card.num should be(0)
+        card.colour should be("green")
+      }
+
+    }
+  }
+
+}
