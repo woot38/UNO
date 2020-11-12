41031a186ccf2e1dd9a41356e8a0dd4a2c55acaa
woot38 <christian.holzinger@htwg-konstanz.de>
2020-11-12 15:00:26 +0100

    TUI ordner und class erstellt

---
+++ src/main/scala/de/htwg/se/simpletui/Tui.scala
@@ -0,0 +1,15 @@
+package de.htwg.se.simpletui
+
+import de.htwg.se.UNO;
+
+class Tui {
+
+
+  /*
+  example input
+  def processInputLine(input: String, grid:Grid):Grid = {
+    input match {}
+  }
+  */
+
+}
